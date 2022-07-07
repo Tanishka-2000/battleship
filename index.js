ee46b969f0eea893670b9c618ac5c73ada6ca6c1
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-07-07 12:59:11 +0530

    game successfully ends on win;

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 // create ship with specific length and its coordinates
 function Ship(shipLength, coordinates){
-    const positionHit = [];
     function hit(n){
-        positionHit.push(n);
+        if(this.positionHit.includes(n)) return false
+        this.positionHit.push(n);
+        return true;
     }
     function isSunk(){
-        return positionHit.length === shipLength;
+        return this.positionHit.length === this.length;
     }
     return {
-        length: () => shipLength,
-        coords: () => [...coordinates],
+        length: shipLength,
+        coords: coordinates,
         hit,
-        isSunk
+        isSunk,
+        positionHit: []
     };
 }
 
 // GameBoard can recieve attack at a cordinate
  function GameBoard(){
      // hardcoded placement of ships
-    ships = [Ship(4,[12,13,14,15]), Ship(3,[7,8,9]), Ship(3,[67,77,87]), Ship(2,[34,35]), Ship(2,[55,65]),
+    let ships = [Ship(4,[12,13,14,15]), Ship(3,[7,8,9]), Ship(3,[67,77,87]), Ship(2,[34,35]), Ship(2,[55,65]),
         Ship(2,[42,52]), Ship(1,[95]), Ship(1,[69]), Ship(1,[38]), Ship(1,[29])];
-    missed = [];
-    sunkedShips = 0;
+    let missed = [];
+    let sankedShips = 0;
     // check for hit and miss
     function recieveAttack(n){
         let attacked = checkForHit(n);
         if(attacked === -1) {
-            missed.push(n);
+            if(this.missed.includes(n)) return;
+            this.missed.push(n);
             return "miss";
         }else{
-            attacked.hit(n);
-            if(attacked.isSunk()) sunkedShips++;
-            return "hit";
+            if(!attacked.hit(n)) return;
+                if(attacked.isSunk()){
+                     this.sankedShips++;
+                 }
+                return "hit";
         }
     }
     // checking if any ship present at attacked coordinates
     function checkForHit(n){
         for (let i = 0; i < 10; i++) {
-            if(ships[i].coords().includes(n)) return ships[i];
+            if(ships[i].coords.includes(n)){
+                return ships[i];
+            }
         }
         return -1;
     }
     // checking all ships sanked
     function allSank(){
-        return ships.length === sunkedShips;
+        return this.ships.length === this.sankedShips;
     }
-    return {recieveAttack, allSank, ships};
+    return {recieveAttack, allSank, ships, missed, sankedShips};
  }
 
-// create genuine player with its gameBoard
-// function RealPlayer(){
-//     const gameBoard = GameBoard();
-//     return{ gameBoard};
-// }
-
-// a computed player has to create random attacks
-function ComputedPlayer(){
+// computed player has to create random attacks
+function ComputedPlayer(randomPosition){
     const attackedPos = [];
-    function randomPosition(){
-        return Math.floor(Math.random()*100) + 1;
-    }
+    let player = 'user';
+
     function createAttack(){
         let n = randomPosition();
         while(attackedPos.includes(n)){
@@ -70,8 +70,15 @@ function ComputedPlayer(){
     }
     return {createAttack};
 }
-function Game(board, gameBoard, heading){
-    const computer = ComputedPlayer();
+
+function randomPosition(){
+    return Math.floor(Math.random()*100) + 1;
+}
+
+function Game(){
+    const computer = ComputedPlayer(randomPosition);
+    let winner;
+
     function createGameBoard(board){
         for (let i = 1; i <= 100; i++) {
             let div = document.createElement('div');
@@ -82,64 +89,89 @@ function Game(board, gameBoard, heading){
     function placeShips(board, gameBoard){
         divs = board.querySelectorAll('div');
         for (var i = 0; i < 10; i++) {
-            ships[i].coords().forEach((item, i) => {
+            gameBoard.ships[i].coords.forEach((item, n) => {
                 divs[item-1].classList.add('ship');
             });
         }
     }
-    function play(e){
-        let position = e.target.getAttribute('data-coord');
-        let result = gameBoard2.recieveAttack(Number(position));
-        e.target.classList.add(result);
-        let elem = e.target.parentElement
-        deactivatePlayer(elem);
-        computerPlay(elem);
-    }
-    function deactivatePlayer(element){
-        heading.textContent = "Computer's turn";
-        element.classList.remove('active');
-        element.removeEventListener('click', game.play);
-    }
+    function start(e){
+        // user's chance
+        play(e.target, computerGameBoard);
+        if(winner) return;
+        deactivatePlayer();
 
-    function activatePlayer(element){
-        heading.textContent = "Player's turn";
-        element.classList.add('active');
-        element.addEventListener('click', game.play);
-    }
-    function computerPlay(otherGameBoard){
+        // computer's chance
+        let choice = computer.createAttack();
+        let divs = board1.querySelectorAll('div');
         setTimeout(function(){
-            let choice = computer.createAttack();
-            let divs = board.querySelectorAll('div');
-            let result = gameBoard1.recieveAttack(choice);
-            divs[choice-1].classList.add(result);
-            activatePlayer(otherGameBoard);
-        },500);
+            play(divs[choice-1], playerGameBoard);
+            if(winner) return;
+            activatePlayer();
+        }, 500);
+    }
+    function play(clickedDiv, currentGameBoard){
+        let position = clickedDiv.getAttribute('data-coord');
+        let result = currentGameBoard.recieveAttack(Number(position));
 
+        if(result) {
+            clickedDiv.classList.add(result);
+            // check for win
+            if(currentGameBoard.allSank()){
+                winner = currentGameBoard === computerGameBoard ? 'player': 'computer';
+                gameOver();
+                return;
+            };
+        }
+    }
+    function deactivatePlayer(){
+        heading.textContent = "Computer's turn";
+        board2.classList.remove('active');
+        board2.removeEventListener('click', start);
     }
 
-    return {createGameBoard, placeShips, play, activatePlayer};
+    function activatePlayer(){
+        heading.textContent = "Player's turn";
+        board2.classList.add('active');
+        board2.addEventListener('click', start);
+    }
+
+    function gameOver(){
+        deactivatePlayer();
+        heading.textContent = winner + ' Wins!';
+    }
+    // function computerPlay(){
+    //         let choice = computer.createAttack();
+    //         let divs = board1.querySelectorAll('div');
+    //         let result = gameBoard1.recieveAttack(choice);
+    //         if(result) divs[choice-1].classList.add(result);
+    //         if(gameBoard1.allSank()){
+    //             winner = 'Computer';
+    //             gameOver();
+    //         }
+    // }
+    return {createGameBoard, placeShips, activatePlayer, winner};
 }
 // module.exports = {
 //     Ship,
 //     GameBoard,
+//     ComputedPlayer
 // };
-//
+
 // Game loop starts here
 const board1 = document.querySelector('.gameBoard1');
 const board2 = document.querySelector('.gameBoard2');
 const heading = document.querySelector('h1');
 
-const gameBoard1 = GameBoard();
-const gameBoard2 = GameBoard();
+const computerGameBoard = GameBoard();
+const playerGameBoard = GameBoard();
 
-
-const game = Game(board1, gameBoard2, heading);
+const game = Game();
 // displaying gameBoard on screnn
 game.createGameBoard(board1);
 game.createGameBoard(board2);
 
 // displayig ships on the gameBoard
-game.placeShips(board1, gameBoard1);
+game.placeShips(board1, playerGameBoard);
 
 // responding to attack when user click on board
-game.activatePlayer(board2)
+game.activatePlayer()
--- index.test.js
+++ index.test.js
@@ -1,17 +1,21 @@
-const {Ship, GameBoard,} = require('./index.js');
+const {Ship, GameBoard, ComputedPlayer} = require('./index.js');
 
 describe('testing ship function', () => {
-    test('ship length always remian same', () => {
+    test('testing coordinates of ships', () => {
         const ship1 = Ship(3,[4,5,6]);
-        expect(ship1.length()).toBe(3);
-        ship1.hit(4);
-        expect(ship1.length()).toBe(3);
+        expect(ship1.length).toBe(3);
+        expect(ship1.coords).toEqual([4,5,6]);
     });
     test('coords remain same', () => {
         const ship1 = Ship(3,[4,5,6]);
-        expect(ship1.coords()).toEqual([4,5,6]);
         ship1.hit(4);
-        expect(ship1.coords()).toEqual([4,5,6]);
+        expect(ship1.positionHit).toEqual([4]);
+    });
+    test('must not hit same coordinates twice', () =>{
+        const ship1 = Ship(3,[4,5,6]);
+        expect(ship1.hit(5)).toBe(true);
+        expect(ship1.hit(5)).toBe(false);
+        expect(ship1.positionHit).toEqual([5]);
     });
     test('test isSunk function', () => {
         const ship1 = Ship(3,[4,5,6]);
@@ -25,25 +29,68 @@ describe('testing ship function', () => {
 
 // test will fail on changing function to accept coordinates from user of selectin random coordinates
 describe('testing gameBoard function with predefined coordinates of ships', () => {
+    const ships = [Ship(4,[12,13,14,15]), Ship(3,[7,8,9]), Ship(3,[67,77,87]), Ship(2,[34,35]), Ship(2,[55,65]),
+             Ship(2,[42,52]), Ship(1,[95]), Ship(1,[69]), Ship(1,[38]), Ship(1,[29])];
+    let gameBoard1 = GameBoard();
     test('getting hit when attacked at ship coordinate', () => {
-        let gameBoard1 = GameBoard();
         expect(gameBoard1.recieveAttack(12)).toBe("hit");
         expect(gameBoard1.recieveAttack(14)).toBe("hit");
-        expect(gameBoard1.recieveAttack(77)).toBe("hit");
-        expect(gameBoard1.recieveAttack(55)).toBe("hit");
-        expect(gameBoard1.recieveAttack(42)).toBe("hit");
         expect(gameBoard1.recieveAttack(95)).toBe("hit");
     });
     test('getting miss when attacked at empty coordinate', () => {
-        let gameBoard1 = GameBoard();
         expect(gameBoard1.recieveAttack(3)).toBe("miss");
         expect(gameBoard1.recieveAttack(22)).toBe("miss");
-        expect(gameBoard1.recieveAttack(37)).toBe("miss");
-        expect(gameBoard1.recieveAttack(66)).toBe("miss");
         expect(gameBoard1.recieveAttack(88)).toBe("miss");
     });
+    test('store missed coordinates', () => {
+        expect(gameBoard1.missed).toEqual([3,22,88]);
+    });
+    test('do not store same coordinate for hit twice', () => {
+        expect(gameBoard1.recieveAttack(12)).toBeUndefined();
+        expect(gameBoard1.missed).toEqual([3,22,88]);
+        expect(gameBoard1.ships[0].positionHit).toEqual([12,14]);
+    });
+    test('do not store same coordinate for miss twice', () => {
+        expect(gameBoard1.recieveAttack(22)).toBeUndefined();
+        expect(gameBoard1.missed).toEqual([3,22,88]);
+    });
+
+    test('store number of sanked ships', () => {
+        gameBoard1.recieveAttack(13);
+        gameBoard1.recieveAttack(15);
+        expect(gameBoard1.sankedShips).toBe(2);
+    });
+
+    test('do not increment sankedShips number for already sankedShip', () => {
+        gameBoard1.recieveAttack(69);
+        expect(gameBoard1.sankedShips).toBe(3);
+        gameBoard1.recieveAttack(69);
+        expect(gameBoard1.sankedShips).toBe(3);
+    });
+
     test('allSank function return false for new gameBoard', () => {
-        let gameBoard1 = GameBoard();
         expect(gameBoard1.allSank()).toBe(false);
+    });
+
+    test('report if all ships sank', () => {
+        ships.forEach( ship => {
+            ship.coords.forEach(coord => {
+                gameBoard1.recieveAttack(coord);
+            });
+        });
+        expect(gameBoard1.sankedShips).toBe(10);
+        expect(gameBoard1.allSank()).toBe(true);
+    });
+});
+
+describe('computer should not generate same attack twice', () => {
+    const mockRandom = jest.fn()
+    mockRandom.mockReturnValueOnce(23).mockReturnValueOnce(30).mockReturnValueOnce(30).mockReturnValueOnce(45);
+    const player = ComputedPlayer(mockRandom);
+
+    test('create unique attack coordinates', () => {
+        expect(player.createAttack()).toBe(23);
+        expect(player.createAttack()).toBe(30);
+        expect(player.createAttack()).toBe(45);
     });
 });
