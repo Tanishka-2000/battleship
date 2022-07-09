6ebf889c6e9a149e02e401fb2a15afa2cf34ac64
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-07-09 15:03:31 +0530

    enable game to restart on game over

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function ComputedPlayer(){
     function createAttack(){
         for(let i = 0; i< 4; i++){
             let temp = getIntelligentHit();
-            if(temp && !attackedPos.includes(temp)){
+            if(temp && !attackedPos.includes(temp) && ((temp >= 1) && (temp <= 100))){
                 attackedPos.push(temp);
                  return temp;
              }
@@ -173,6 +173,7 @@ function Game(){
 
         // computer's chance
         let choice = computer.createAttack();
+        console.log(choice);
         let divs = board1.querySelectorAll('div');
         setTimeout(function(){
             play(divs[choice-1], playerGameBoard);
@@ -219,7 +220,19 @@ function Game(){
         cover.children[0].textContent = winner + 'Wins!';
         withComputer.textContent = 'Restart Game';
     }
-    return {createGameBoard, placeShips, activatePlayer, winner};
+    function clearBoard(){
+        board1.style.opacity = '1';
+        board2.innerHTML = '';
+        let divs = board1.querySelectorAll('div');
+        divs.forEach(div => {
+            div.className = '';
+        });
+         divs = board2.querySelectorAll('div');
+        divs.forEach(div => {
+            div.className = '';
+        });
+    }
+    return {createGameBoard, placeShips, activatePlayer, winner, clearBoard, gameOver};
 }
 
 
@@ -266,100 +279,112 @@ const game = Game();
 game.createGameBoard(board1);
 const divs = board1.querySelectorAll('div');
 
-const computerGameBoard = GameBoard(createShips());
-let playerGameBoard;
+let computerGameBoard, playerGameBoard;
 
 withComputer.addEventListener('click', function(){
     cover.style.display = 'none';
+    game.clearBoard();
+    computerGameBoard = GameBoard(createShips());
+    letUserPlaceShips();
 });
 
 // withPlayer.addEventListener('click', function(){
 //     cover.style.display = 'none';
 // });
+function letUserPlaceShips(){
+    let takenCoords = [];
+    const lengths = [4,3,3,2,2,2,1,1,1,1];
+    let n = 1;
+    shipNum = 0;
+    let coords = [];
+    const ships = [];
+    const shipType = {
+        4:'Carrier Ship',
+        3:'BattleShip',
+        2:'Submarine',
+        1:'Petrol Boat'
+    };
 
-let takenCoords = [];
-const lengths = [4,3,3,2,2,2,1,1,1,1];
-let n = 1;
-shipNum = 0;
-let coords = [];
-const ships = [];
-const shipType = {
-    4:'Carrier Ship',
-    3:'BattleShip',
-    2:'Submarine',
-    1:'Petrol Boat'
-};
+    const horizontal = document.querySelector('.horizontal');
+    const vertical = document.querySelector('.vertical');
 
-const horizontal = document.querySelector('.horizontal');
-const vertical = document.querySelector('.vertical');
+    horizontal.addEventListener('click', () => n = 1);
+    vertical.addEventListener('click',() => n = 10);
 
-horizontal.addEventListener('click', () => n = 1);
-vertical.addEventListener('click',() => n = 10);
+    board1.addEventListener('mouseover', showPossibleCoords);
+    board1.addEventListener('mouseout', hidePossibleCoords);
+    board1.addEventListener('click', placeShip);
 
-board1.addEventListener('mouseover', showPossibleCoords);
-board1.addEventListener('mouseout', hidePossibleCoords);
-board1.addEventListener('click', placeShip);
-
-heading.textContent = 'Place your '+ shipType[lengths[shipNum]];
-
-function showPossibleCoords(e){
-    coords = [];
-    let pos = Number(e.target.getAttribute('data-coord'));
-
-    for(let i = 0; i < lengths[shipNum]; i++){
-        coords.push(pos + (n*i));
-    }
-
-    if(!validateCoords()) return;
-
-    coords.forEach(i => {
-        divs[i - 1].classList.add('hovered');
-    });
-}
-
-function hidePossibleCoords(){
-    if(!coords || !validateCoords()) return;
-    coords.forEach(i => {
-        divs[i - 1].classList.remove('hovered');
-    });
-}
-
-function placeShip(){
-    if(!validateCoords()) return;
-    ships.push(new Ship(coords.length, coords));
-
-    coords.forEach(i => {
-        divs[i - 1].classList.add('ship');
-    });
-    takenCoords = [...takenCoords, ...coords];
-    shipNum++;
     heading.textContent = 'Place your '+ shipType[lengths[shipNum]];
-    if(shipNum === 10) startGame();
-}
 
-function validateCoords(){
-    if(coords.some(i => takenCoords.includes(i))) return false;
-    let x = parseInt(coords[0]/10);
-    let y = coords[0]%10;
-    if(y === 0){
-        x--;
-        y = 10;
+    function showPossibleCoords(e){
+        coords = [];
+        let pos = Number(e.target.getAttribute('data-coord'));
+
+        for(let i = 0; i < lengths[shipNum]; i++){
+            coords.push(pos + (n*i));
+        }
+
+        if(!validateCoords()) return;
+
+        coords.forEach(i => {
+            divs[i - 1].classList.add('hovered');
+        });
     }
-    if((n === 1) && (y > (10 - coords.length + 1))) return false;
-    if((n === 10) && (x > (10 - coords.length))) return false;
-    return true;
+
+    function hidePossibleCoords(){
+        if(!coords || !validateCoords()) return;
+        coords.forEach(i => {
+            divs[i - 1].classList.remove('hovered');
+        });
+    }
+
+    function placeShip(){
+        if(!validateCoords()) return;
+        ships.push(new Ship(coords.length, coords));
+
+        coords.forEach(i => {
+            divs[i - 1].classList.add('ship');
+        });
+        takenCoords = [...takenCoords, ...coords];
+        shipNum++;
+        heading.textContent = 'Place your '+ shipType[lengths[shipNum]];
+        if(shipNum === 10) startGame();
+    }
+
+    function validateCoords(){
+        if(coords.some(i => takenCoords.includes(i))) return false;
+        let x = parseInt(coords[0]/10);
+        let y = coords[0]%10;
+        if(y === 0){
+            x--;
+            y = 10;
+        }
+        if((n === 1) && (y > (10 - coords.length + 1))) return false;
+        if((n === 10) && (x > (10 - coords.length))) return false;
+        return true;
+    }
+    function resetForNextGame(){
+        takenCoords = [];
+        n = 1;
+        shipNum = 0;
+        coords = [];
+        ships.length = 0;
+    }
+    function startGame(){
+        board1.removeEventListener('mouseover', showPossibleCoords);
+        board1.removeEventListener('mouseout', hidePossibleCoords);
+        board1.removeEventListener('click', placeShip);
+        horizontal.parentElement.style.display = 'none';
+
+        let newShips = [...ships];
+        playerGameBoard = new GameBoard(newShips);
+        resetForNextGame();
+        game.createGameBoard(board2);
+        game.activatePlayer();
+    }
 }
 
-function startGame(){
-    board1.removeEventListener('mouseover', showPossibleCoords);
-    board1.removeEventListener('mouseout', hidePossibleCoords);
-    board1.removeEventListener('click', placeShip);
-    horizontal.parentElement.style.display = 'none';
-
-    playerGameBoard = new GameBoard(ships);
-    game.createGameBoard(board2);
-    game.activatePlayer();
-}
 // module.exports = {
 //     Ship,
 //     GameBoard,
